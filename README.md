913587de409216d68c57f752393dc2adfdcfc075
Maria Luiza <co.maria.luiza@gmail.com>
2020-04-03 21:27:59 -0300

    Core functionality working

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Github Overview
 
-Overview do Github. [Inspiração.](https://github.com/florinpop17/app-ideas/blob/master/Projects/2-Intermediate/GitHub-Profiles.md)
+Overview do Github, inspirado [nesse](https://github.com/florinpop17/app-ideas/blob/master/Projects/2-Intermediate/GitHub-Profiles.md) desafio. O usuário digita um username de um usuário válido do github e recebe a foto, nome, bio e link para o perfil.
+
+## Funcionalidades
+- [x] Pesquisar o usuário na API do GitHub
+- [ ] Retornar usuário com foto, nome, bio e link para o perfil.
+- [ ] Retornar quantidade de repositórios
+- [ ] Retornar últimos quatro repositórios
+- [ ] Pesquisar following e followers na API do GitHub
+- [ ] Retornar um erro se o usuário não existir
+- [ ] Implantar dark mode e persistir mesmo ao fechar a aplicação
+
+![](/gitImages/projectOverview.png)
+
+## Tecnologias utilizadas
+React
+Axios
+CSS
--- src/App.js
+++ src/App.js
@@ -1,35 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import api from './services/api';
 
 import './style.css';
 
 function App() {
-  const [ user, setUser ] = useState(''); 
 
-  async function getUserInfo(e) {
-    e.preventDefault();
+  const [ user, setUser ] = useState('');
+  const [ userImg, setUserImg ] = useState('');
+  const [ userFollowing, setUserFollowing ] = useState('');
+  const [ userFollowers, setUserFollowers ] = useState('');
+  const [ userBio, setUserBio ] = useState('');
+  const [ userHireable, setUserHireable ] = useState('');
+  const [ userLink, setUserLink ] = useState('');
 
+  const getUser = async (e) => {
     try {
-      const res = await api.get(`${user}`);
-      console.log(res);
-    } catch(err) {
+      e.preventDefault();
+      const user = e.target.elements.username.value;
+
+      if (user) {
+        const res = await api.get(`${user}`);
+        
+        const userInfo = res.data;
+        console.log(res);
+  
+        const userName = userInfo.name;
+        const userImg = userInfo.avatar_url;
+        const userFollowing = userInfo.following_url;
+        const userFollowers = userInfo.followers_url;
+        const userBio = userInfo.bio;
+        const userHireable = userInfo.hireable;
+        const userLink = userInfo.html_url;
+  
+        setUser(userName);
+        setUserImg(userImg);
+        setUserFollowing(userFollowing);
+        setUserFollowers(userFollowers);
+        setUserBio(userBio);
+        setUserHireable(userHireable);
+        setUserLink(userLink);
+      } 
+
+      return;
+    } catch(err) { 
       console.log(err);
     }
   }
-
+  
   return (
     <div className="mainContainer">
-      <div className="container">
-        <h1 className="mainTitle">Github Overview</h1>
+      <div className="content">
+        <div className="container formContainer">
+          <h1 className="mainTitle">Github Overview</h1>
 
-        <form onSubmit={ getUserInfo } className="getUserInfo">
-          <input 
-            placeholder="Github username"
-            value={ user }
-            onChange={ e => setUser(e.target.value) }
-          />
-          <button type="submit">Send</button>
-        </form>
+          <form onSubmit={ getUser } className="getUserInfo">
+            <input 
+              placeholder="Github username"
+              name="username"
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+
+        <div className="container overViewContainer">
+          { user 
+            ? 
+              <div className="userInfoBlock">
+                <div className="userInfoVisual">
+                  <img src={ userImg } alt={ user } className="userInfoImg"/>
+                </div>
+
+                <div className="userInfoDesc">
+                  <p className="userInfoName">{ user }</p>
+                  <p className="userInfoBio">{ userBio }</p>
+
+                  <a href={ userLink } className="userInfoLink">Contact</a>
+                </div>
+              </div>
+            : 
+              <Fragment />
+          }
+        </div>
       </div>
     </div>
   );
